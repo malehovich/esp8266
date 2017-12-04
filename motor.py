e0b98d91b0d5f8b4e90a3eca22edc602031e56b9
maleha <maleha@mega-apteka.com>
2017-12-04 17:12:30 +0200

    [[[yz

--- motor.py
+++ motor.py
@@ -1,9 +1,9 @@
 def close()
-    import uln2003
+    import uln
     s1 = Stepper(HALF_STEP, microbit.pin16, microbit.pin15, microbit.pin14, microbit.pin13, delay=5)
     s1.step(100)
 
 def open()
-    import uln2003
+    import uln
     s1 = Stepper(HALF_STEP, microbit.pin16, microbit.pin15, microbit.pin14, microbit.pin13, delay=5)
     s1.step(100,-1)
