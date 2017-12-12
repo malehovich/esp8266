780adbfa48675337494e8f415537326adeb764b9
maleha <maleha@mega-apteka.com>
2017-12-12 16:15:42 +0200

    [[[yz

--- driver.py
+++ driver.py
@@ -15,16 +15,8 @@ HALF_STEP = [
     [HIGH, LOW, LOW, HIGH],
 ]
 
-
-def reset (pn1,pn2,pn3,pn4):
-    p1 = Pin(pn1, Pin.OUT, value=0)
-    p2 = Pin(pn2, Pin.OUT, value=0)
-    p3 = Pin(pn3, Pin.OUT, value=0)
-    p4 = Pin(pn4, Pin.OUT, value=0)
-
-
 def step(direction,count,pn1,pn2,pn3,pn4):
-    reset(pn1,pn2,pn3,pn4)
+
     for x in range(count):
         for bit in HALF_STEP[::direction]:
             p1 = Pin(pn1, Pin.OUT, value=bit[0])
@@ -32,5 +24,8 @@ def step(direction,count,pn1,pn2,pn3,pn4):
             p1 = Pin(pn3, Pin.OUT, value=bit[2])
             p1 = Pin(pn4, Pin.OUT, value=bit[3])
             time.sleep_us(15)
+p1 = Pin(pn1, Pin.OUT, value=0)
+p2 = Pin(pn2, Pin.OUT, value=0)
+p3 = Pin(pn3, Pin.OUT, value=0)
+p4 = Pin(pn4, Pin.OUT, value=0)
 
-reset(pn1,pn2,pn3,pn4)
